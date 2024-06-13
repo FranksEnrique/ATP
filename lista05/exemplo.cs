73917e55a6c087167677d768ee923e2243828fba
Franks Enrique Carvalho de Castro <1526451@sga.pucminas.br>
2024-06-12 22:11:02 -0300

    Lista_5

--- lista05/exemplo.cs
+++ lista05/exemplo.cs
@@ -1,6 +1,20 @@
-Using system;
-public class exemplo {
-    public static void main() {
-        console.writeLine("Olá, Mundo");
-    }
+Using system; 
+
+
+class Program
+{
+
+public static void Main(){
+    condole.WriteLine("Escolha um exercicío da lista 5 (de um a 10): ");
+    int num = int.Parse(Console.ReadLine());
+     
+     switch (num)
+     {
+     case 1:
+     Exercicío_01_lista_05 exercicío_01_lista_05 = new Exercicío_01_lista_05();
+     exercicío_01_lista_05.Rodar();
+     break;
+     }
+
+}
 }
